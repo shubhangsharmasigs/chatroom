16c7f806fe8872fb8439802ca9c224d6363ee441
shubhangsharmasigs <shubhangsharmasigs@gmail.com>
2022-01-23 13:41:12 +0530

    initial commit setup

---
+++ chatroom/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAzQjgp9GgwaMg3swFRfNjiQQHNOsd2rfs",
+    authDomain: "chatroom-react-web.firebaseapp.com",
+    projectId: "chatroom-react-web",
+    storageBucket: "chatroom-react-web.appspot.com",
+    messagingSenderId: "558006827076",
+    appId: "1:558006827076:web:978eeef6fe86beaf986411",
+    measurementId: "G-EXCVP0DEHZ"
+  };
